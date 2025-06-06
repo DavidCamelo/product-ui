9ec5d633bb6365e6cdd6adf693b71f9aeae3bfca
David Camelo <dacaro16@gmail.com>
2025-06-06 18:15:19 -0500

    Setup Product UI

--- src/product/Product.jsx
+++ src/product/Product.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import Input from "components_ui/Input";
-import List from "components_ui/List";
+import { InputTest } from "components_ui/InputTest"
+import { List } from "components_ui/List"
 
 function Product() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Input value={count} onChange={setCount} onSubmit={() => setCount((count) => count + 1)} />
+      <InputTest value={count} onChange={setCount} onSubmit={() => setCount((count) => count + 1)} />
       <List items={["Product UI", "Learn Vite", "Make an awesome app"]} />
     </>
   )
