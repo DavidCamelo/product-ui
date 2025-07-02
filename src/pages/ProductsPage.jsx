110d62d23cdad3472d79cc5b278a51d1f6d12b94
David Camelo <dacaro16@gmail.com>
2025-07-02 09:48:56 -0500

    Setup Product UI

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { ResourcePage } from 'components_ui/ResourcePage';
 import { Spinner } from 'components_ui/Spinner';
 import { productService, userService } from 'components_ui/api';
-import './products-page.css';
 
 export const ProductsPage = () => {
     const [users, setUsers] = useState([]);
